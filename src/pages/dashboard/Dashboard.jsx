7f58434eb2249598344153f22c822b6470e733da
ItsZeroFour <itsZeroFourX@gmail.com>
2024-12-15 14:23:06 +0300

    add popular content page and some animation

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import style from "./style.module.scss";
 import General from "./General";
+import PopularConnent from "./PopularConnent";
 
 const Dashboard = () => {
   return (
     <React.Fragment>
       <General />
+
+      <PopularConnent />
     </React.Fragment>
   );
 };
